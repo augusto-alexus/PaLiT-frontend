e56030c09ff32195365e2d338a77ea0c6b2cb9e4
augusto-alexus <oleksandr.sentypal.knm.2020@lpnu.ua>
2024-02-15 21:30:58 +0200

    conf

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 
 export default defineConfig({
+    base: '/PaLiT-frontend/',
     resolve: {
         alias: {
             '~': path.resolve(__dirname, './src'),
